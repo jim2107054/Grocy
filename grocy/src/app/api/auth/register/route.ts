492b0948fd346cd240db2c5f6a4b99eaebf1c7ac
jim2107054 <mdjahid205hasan@gmail.com>
2025-12-08 17:21:31 +0600

    feat: implement user registration API with validation and password hashing

--- grocy/src/app/api/auth/register/route.ts
+++ grocy/src/app/api/auth/register/route.ts
@@ -1,3 +1,63 @@
-export async function POST(request: Request) {
-  return new Response("Register API Route");
+import connectDB from "@/lib/db";
+import UserModel from "@/models/userModel";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+/*
+  ! Handle user registration
+  * @param {NextRequest} req - The incoming request object
+  * @param {NextResponse} res - The outgoing response object 
+  !In first object -> we pass the data, in second object -> we set the status code.
+*/
+export async function POST(req: NextRequest) {
+  try {
+    //Need to connect to the database here
+    await connectDB();
+
+    const { name, email, password } = await req.json();
+
+    // Basic validation
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { message: "Name, email, and password are required" },
+        { status: 400 }
+      );
+    }
+
+    // existing user check would go here
+    const existUser = await UserModel.findOne({ email });
+    if (existUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 409 }
+      );
+    }
+
+    // check password length
+    if (password.length < 6) {
+      return NextResponse.json(
+        { message: "Password must be at least 6 characters long" },
+        { status: 400 }
+      );
+    }
+
+    // hash password before saving to database
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await UserModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return NextResponse.json(
+      { message: "User registered successfully", user: newUser },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
