8b88bbca1540177713c6f7ba5d1998beb9ca7cfc
jim2107054 <mdjahid205hasan@gmail.com>
2025-12-08 11:59:09 +0600

    feat: add global mongoose connection handling and type definitions

---
+++ grocy/src/global.d.ts
@@ -0,0 +1,11 @@
+import { Connection } from "mongoose";
+
+//custom types for global usage
+declare global {
+    var mongoose: {
+        conn : Connection | null;
+        promise : Promise<Connection> | null;
+    }
+}
+
+export {};
--- grocy/src/lib/db.ts
+++ grocy/src/lib/db.ts
@@ -1,1 +1,36 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
+const mongodbUri = process.env.MONGODB_URI;
+
+if (!mongodbUri) {
+  throw new Error("MONGODB_URI is not defined in environment variables");
+}
+
+let cache = global.mongoose;
+
+if (!cache) {
+  cache = global.mongoose = { conn: null, promise: null };
+}
+
+const connectDB = async () => {
+  if (cache.conn) {
+    return cache.conn; // Use existing connection, we are already connected. Don't create a new connection.
+  }
+
+  // if we have no cache and no promise, create a new connection
+  if (!cache.promise) {
+    cache.promise = mongoose
+      .connect(mongodbUri)
+      .then((conn) => conn.connection);
+  }
+
+  //But we have a promise to connect, wait for it
+  try {
+    const conn = await cache.promise;
+    return conn;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectDB;
